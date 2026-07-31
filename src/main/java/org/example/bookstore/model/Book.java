9a7a5e751c7174a322043884a61b9cc30d746dc9
Slawomir Pacek <slawomir_pacek@op.pl>
2026-07-31 13:33:17 +0200

    Add Book model class

---
+++ src/main/java/org/example/bookstore/model/Book.java
@@ -0,0 +1,28 @@
+package org.example.bookstore.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String description;
+
+    private String coverImage;
+
+}
