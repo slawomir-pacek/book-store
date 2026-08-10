52c3730861dfbea98e33e8fd054b07d8b7b07386
Slawomir Pacek <slawomir_pacek@op.pl>
2026-08-10 15:12:32 +0200

    task2

--- src/main/java/org/example/bookstore/BookStoreApplication.java
+++ src/main/java/org/example/bookstore/BookStoreApplication.java
@@ -1,12 +1,7 @@
 package org.example.bookstore;
 
-import java.math.BigDecimal;
-import org.example.bookstore.model.Book;
-import org.example.bookstore.service.BookService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -15,22 +10,4 @@ public class BookStoreApplication {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(final BookService bookService) {
-        return args -> {
-            Book book = new Book();
-
-            book.setTitle("Clean Code");
-            book.setAuthor("Robert C. Martin");
-            book.setIsbn("9780132350884");
-            book.setPrice(BigDecimal.valueOf(49.99));
-
-            bookService.save(book);
-
-            System.out.println("All books:");
-
-            bookService.findAll()
-                    .forEach(System.out::println);
-        };
-    }
 }
