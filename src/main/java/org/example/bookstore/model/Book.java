f3810799f1de884bbb768deabb812af06d69f718
Slawomir Pacek <slawomir_pacek@op.pl>
2026-08-14 08:41:23 +0200

    task3-uwzglednione-uwagi

--- src/main/java/org/example/bookstore/model/Book.java
+++ src/main/java/org/example/bookstore/model/Book.java
@@ -14,7 +14,7 @@ import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
-@SQLDelete(sql = "UPDATE book SET is_deleted = true WHERE id=?")
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
 @Entity
 @Table(name = "books")
