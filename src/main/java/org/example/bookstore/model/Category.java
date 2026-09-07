ac6f4d23849ce5f0a3633d0cf6a04165afa13a9d
Slawomir Pacek <slawomir_pacek@op.pl>
2026-09-07 11:34:36 +0200

    adding-category

---
+++ src/main/java/org/example/bookstore/model/Category.java
@@ -0,0 +1,19 @@
+package org.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+}
